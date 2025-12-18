c6faf40f258eda08df19049661c040e91854e44d
Guilherme de Paula <guilhermedepaula.ti@gmail.com>
2025-12-18 20:59:39 -0300

    refactor: remove React import from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { ViewMode } from './types/game';
 import { useGameStorage } from './hooks/useGameStorage';
 import { useCardGenerator } from './hooks/useCardGenerator';
